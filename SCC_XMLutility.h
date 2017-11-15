b0d93697f8142ba66ba33bcd57217db2cedf2717
Chris Anderson <anderson@math.ucla.edu>
2017-11-15 10:13:24 -0800

    added copyright info

--- SCC_XMLutility.h
+++ SCC_XMLutility.h
@@ -4,10 +4,35 @@
  *  Created on: Apr 15, 2016
  *      Author: anderson
  */
+
+
 //
 // Utility functions for SCC class operators
 // involving XML_ParameterListArray
 //
+
+/*
+#############################################################################
+#
+# Copyright 2017 Chris Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+*/
+
+
 #ifndef _SCC_XMLutilty_
 #define _SCC_XMLutilty_
 
